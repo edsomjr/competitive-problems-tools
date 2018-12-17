4e05341315ee21f110674160b36e50517a4e14da
Edson Alves <edsomjr@gmail.com>
2018-12-17 18:53:49 -0200

    Adição do módulo principal (cptools).

---
+++ include/cptools.h
@@ -0,0 +1,14 @@
+#ifndef CP_TOOLS_H
+#define CP_TOOLS_H
+
+#include <iostream>
+
+
+namespace cptools {
+
+    // Main routine
+    int run(int argc, char * const argv[], std::ostream& out, std::ostream& err);
+
+}
+
+#endif
