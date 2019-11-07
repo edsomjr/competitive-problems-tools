f93795f1e8c1145c90b9948c10365436225085ef
Edson Alves <edsomjr@gmail.com>
2019-11-06 21:04:03 -0300

    Remoção do output da função cptools::sh::copy_dir.

--- src/sh.cpp
+++ src/sh.cpp
@@ -39,7 +39,6 @@ namespace cptools::sh {
                     std::filesystem::copy(p.path(), name);
             } catch (const std::exception& e)
             {
-                std::cerr << e.what() << '\n';
                 return CP_TOOLS_ERROR_SH_COPY_FILES;
             }
         }
