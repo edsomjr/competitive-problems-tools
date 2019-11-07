8fdce6500a473f3b75b1fc1f557bbfb82c6da5d8
Edson Alves <edsomjr@gmail.com>
2019-11-07 17:35:48 -0300

    Adição de opções na ação gentex do comando problem.

--- src/gentex.cpp
+++ src/gentex.cpp
@@ -5,8 +5,10 @@
 
 #include <unistd.h>
 #include <getopt.h>
+#include <dirent.h>
 
 #include "defs.h"
+#include "dirs.h"
 #include "init.h"
 #include "util.h"
 #include "error.h"
@@ -35,11 +37,18 @@ Generate a LaTeX file from the problem description. The options are:
 
     -l              List all available document classes.
     --list
+
+    --no_author     Don't show problem's author.
+
+    --no_contest    Don't show problem's contest.
+
 )message" };
 
 
 namespace cptools::gentex {
 
+    typedef enum { NO_AUTHOR = 1000, NO_CONTEST } Value;
+    
     // Global variables
     static struct option longopts[] = {
         { "help", no_argument, NULL, 'h' },
@@ -48,12 +57,21 @@ namespace cptools::gentex {
         { "list", no_argument, NULL, 'l' },
         { "class", required_argument, NULL, 'c' },
         { "output", required_argument, NULL, 'o' },
+        { "no_author", no_argument, NULL, NO_AUTHOR },
+        { "no_contest", no_argument, NULL, NO_CONTEST },
         { 0, 0, 0, 0 }
     };
 
     static const std::map<std::string, std::string> languages {
         { "en_US", "english" },
         { "pt_BR", "portuguese" },
+    };
+
+    struct Flags {
+        bool include_author;
+        bool include_contest;
+
+        Flags() : include_author(true), include_contest(true) {}
     };
 
     // Auxiliary routines
@@ -70,61 +88,66 @@ namespace cptools::gentex {
     int list_document_classes(std::ostream& out, std::ostream& err)
     {
         const std::string document_classes_dir { "/usr/local/lib/" NAME "/classes" };
+        DIR *d = opendir(CP_TOOLS_CLASSES_DIR);
+
+        if (d == nullptr)
+        {
+            err << "Directory '" << CP_TOOLS_CLASSES_DIR << "' does not exists\n";
+            return CP_TOOLS_ERROR_GENTEX_LIST_DOCUMENT_CLASSES;
+        }
 
         out << '\n';
+        out << "    Class           Description\n\n";
 
-        try {
-            for (const auto& it : std::filesystem::directory_iterator(document_classes_dir))
-            {
-                if (not it.is_regular_file())
-                    continue;
-
-                auto filename = it.path().filename(); 
-                auto tokens = split(filename, '.');
-
-                if (tokens.size() != 2)
-                    continue;
-
-                auto name = strip(tokens[0], '"');
-                auto ext = strip(tokens[1], '"');
-
-                if (ext != "cls" or name.empty() or name.front() == '.')
-                    continue;
-
-                std::ifstream in(it.path().string());
-                std::string line;
-
-                if (!in)
-                    continue;
-
-                getline(in, line);
-
-                if (line.empty())
-                    continue;
-
-                // % is the LaTeX line comment char
-                auto pos = line.find('%');
-
-                if (pos == std::string::npos)
-                    continue;
-
-                line = strip(line.substr(pos + 1));
-
-                out << "    " << name;
-                int count = 4 + name.size();
-
-                while (count < 20)
-                {
-                    out << ' ';
-                    ++count;
-                }
-
-                out << line << '\n';
-            }
-        } catch (const std::exception& e)
+        while (auto dir = readdir(d))
         {
-            err << e.what() << '\n';
-            return CP_TOOLS_ERROR_GENTEX_LIST_DOCUMENT_CLASSES;
+            if (dir == nullptr)
+                break;
+
+            if (dir->d_name[0] == '.')
+                continue;
+
+            std::string filename { dir->d_name };
+            auto tokens = split(filename, '.');
+
+            if (tokens.size() != 2)
+                continue;
+
+            auto name = strip(tokens[0], '"');
+            auto ext = strip(tokens[1], '"');
+
+            if (ext != "cls" or name.empty() or name.front() == '.')
+                continue;
+
+            std::ifstream in(std::string(CP_TOOLS_CLASSES_DIR) + "/" + filename);
+            std::string line;
+
+            if (!in)
+                continue;
+
+            getline(in, line);
+
+            if (line.empty())
+                continue;
+
+            // % is the LaTeX line comment char
+            auto pos = line.find('%');
+
+            if (pos == std::string::npos)
+                continue;
+
+            line = strip(line.substr(pos + 1));
+
+            out << "    " << name;
+            int count = 4 + name.size();
+
+            while (count < 20)
+            {
+                out << ' ';
+                ++count;
+            }
+
+            out << line << '\n';
         }
 
         out << '\n';
@@ -132,23 +155,31 @@ namespace cptools::gentex {
         return CP_TOOLS_OK;
     }
 
-    int generate_latex(const std::string& doc_class, const std::string& language,
-        const std::string& label, std::ostream& out)
+    int generate_latex(const std::string& doc_class, const std::string& language, 
+        const Flags& flags, const std::string& label, std::ostream& out)
     {
         auto config = config::read("config.json");
 
-        auto lang = languages.at(language);
+        auto lang { languages.at(language) };
         auto event { config::get(config, "problem|contest", std::string()) }; 
-        auto title = config::get(config, "problem|title|" + language, std::string("Título"));
-        auto timelimit = config::get(config, "problem|timelimit", 1.0);
+        auto author { config::get(config, "problem|author", std::string()) }; 
+        auto title { config::get(config, "problem|title|" + language, std::string("Título")) };
+        auto timelimit { config::get(config, "problem|timelimit", 1.0) };
         int memorylimit = round(config::get(config, "problem|memory_limit", 256.0));
 
         auto c1_size = config::get(config, "PDF|first_column_size", std::string("6cm"));
         auto c2_size = config::get(config, "PDF|second_column_size", std::string("8cm"));
 
+        if ((not flags.include_contest) or (not config::get(config, "PDF|include_contest", false)))
+            event = "";
+
+        if ((not flags.include_author) or (not config::get(config, "PDF|include_author", false)))
+            author = "";
+
         out << "\\documentclass[" << lang << "]{" << doc_class << "}\n\n";
-        out << "\\event{" << event << "}\n\n";
         out << "\\begin{document}\n\n";
+
+        out << "\\header{" << event << "}{" << author << "}\n\n";
 
         out.precision(1);
         out << "\\begin{problem}{" << label << "}{" << title << "}{" << std::fixed 
@@ -170,6 +201,8 @@ namespace cptools::gentex {
         out << "\\begin{problemnotes}{tex/" << language << "/notes}\n";
         out << "\\end{problemnotes}\n\n";
 
+        out << "\\trailer{" << event << "}{" << author << "}\n\n";
+
         out << "\\end{problem}\n";
         out << "\\end{document}\n";
 
@@ -182,6 +215,7 @@ namespace cptools::gentex {
         int option = -1;
 
         std::string document_class { "cp_modern" }, outfile, language { "en_US" }, label { "A" };
+        Flags flags;
 
         while ((option = getopt_long(argc, argv, "ho:c:lg:b:", longopts, NULL)) != -1)
         {
@@ -219,6 +253,14 @@ namespace cptools::gentex {
                 break;
             }
 
+            case NO_AUTHOR:
+                flags.include_author = false;
+                break;
+
+            case NO_CONTEST:
+                flags.include_contest = false;
+                break;
+
             default:
                 err << help() << '\n';
                 return CP_TOOLS_ERROR_GENTEX_INVALID_OPTION;
@@ -232,10 +274,10 @@ namespace cptools::gentex {
             if (!of)
                 return CP_TOOLS_ERROR_GENTEX_INVALID_OUTFILE;
 
-            return generate_latex(document_class, language, label, of);
+            return generate_latex(document_class, language, flags, label, of);
         }
 
-        return generate_latex(document_class, language, label, out);
+        return generate_latex(document_class, language, flags, label, out);
     }
 
 }
