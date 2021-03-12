2c8f1e3e081f34df939538b2cdafe0a1e77b2591
Vitor Falcao <vitorfhc@protonmail.com>
2021-03-11 20:01:53 -0500

    Add tests for when Polygon command is successful

--- tests/polygon.cpp
+++ tests/polygon.cpp
@@ -7,6 +7,7 @@
 #include "commands/polygon.h"
 #include "error.h"
 #include "fs.h"
+#include "util.h"
 
 using namespace std::filesystem;
 using cptools::commands::polygon::run;
@@ -52,12 +53,6 @@ SCENARIO("Command polygon", "[polygon]") {
           REQUIRE(rc == CP_TOOLS_ERROR_POLYGON_CANT_CONNECT);
         }
       }
-
-      AND_WHEN("There's a valid credentials file in user's home") {
-        THEN("The output must be a connection successful message") {
-          /* TODO: find a way to test valid credentials in the CI and here */
-        }
-      }
     }
 
     WHEN("The credentials file and command line credentials are specified") {
@@ -88,9 +83,60 @@ SCENARIO("Command polygon", "[polygon]") {
       }
     }
 
-    WHEN("Valid credentials are passed in the command line") {
-      THEN("The output must be a connection successful message") {
-        /* TODO: find a way to test valid credentials in the CI and here */
+    WHEN("There are valid credentials") {
+      // Load valid credentials giving priotity to environtment variables
+      auto polygon_key_env = getenv("POLYGON_KEY");
+      auto polygon_secret_env = getenv("POLYGON_SECRET");
+      bool use_env = polygon_key_env && polygon_secret_env;
+
+      string polygon_key, polygon_secret;
+      if (not use_env) {
+        auto config = cptools::util::read_json_file(config_bkp);
+        polygon_key =
+            cptools::util::get_json_value<string>(config, "polygon|key", "");
+        polygon_secret =
+            cptools::util::get_json_value<string>(config, "polygon|secret", "");
+      } else {
+        polygon_key = string(polygon_key_env);
+        polygon_secret = string(polygon_secret_env);
+      }
+
+      AND_WHEN("They are in a valid credentials file in user's home") {
+        // Insert credentials in the valid json config file
+        auto valid_json = "{\"polygon\":{\"key\":\"" + polygon_key +
+                          "\",\"secret\":\"" + polygon_secret + "\"}}";
+
+        if (exists(config_path))
+          remove(config_path);
+        std::ofstream valid_config(config_path);
+        valid_config << valid_json;
+        valid_config.close();
+
+        THEN("The output must be a connection successful message") {
+          int argc = 2;
+          char *const argv[]{(char *)"cp-tools", (char *)"polygon"};
+          optind = 1;
+
+          std::ostringstream out, err;
+          auto rc = run(argc, argv, out, err);
+
+          REQUIRE(rc == CP_TOOLS_OK);
+        }
+      }
+
+      AND_WHEN("Valid credentials are passed in the command line") {
+        THEN("The output must be a connection successful message") {
+          int argc = 6;
+          char *const argv[]{(char *)"cp-tools", (char *)"polygon",
+                             (char *)"-s",       (char *)polygon_secret.c_str(),
+                             (char *)"-k",       (char *)polygon_key.c_str()};
+          optind = 1;
+
+          std::ostringstream out, err;
+          auto rc = run(argc, argv, out, err);
+
+          REQUIRE(rc == CP_TOOLS_OK);
+        }
       }
     }
   }
