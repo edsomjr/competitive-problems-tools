0117f6bb8e6cf3d14740693a3892b859c9af8c48
Vitor Falcao <vitorfhc@protonmail.com>
2021-04-02 21:05:26 -0300

    Creates several functions to modify config.json
    This commit:
    - Creates the function config::modify_config_file
    - Creates the function util::to_json_pointer
    - Creates the function util::create_json_operation

--- include/config.h
+++ include/config.h
@@ -3,9 +3,13 @@
 
 #include <string>
 
+#include "fs.h"
 #include "json.hpp"
+#include "util.h"
 
 namespace cptools::config {
+
+const std::string config_path_name = "config.json";
 
 nlohmann::json read_config_file();
 std::string get_polygon_problem_id(const nlohmann::json &json_object);
@@ -13,6 +17,16 @@ std::string get_tool_file_name(const nlohmann::json &json_object, const std::str
 
 std::vector<std::string> get_solutions_file_names(const nlohmann::json &json_object,
                                                   const std::string &tag);
+
+template <typename T>
+void modify_config_file(const std::string &path, T new_value, const std::string &op = "replace") {
+    auto pointer = util::to_json_pointer(path);
+    auto json_patch = util::create_json_operation(pointer, op, new_value);
+
+    auto config = read_config_file();
+    auto patched = config.patch(json_patch);
+    fs::overwrite_file(config_path_name, patched.dump(4));
+}
 
 } // namespace cptools::config
 
