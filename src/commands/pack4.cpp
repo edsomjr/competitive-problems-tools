dd7aa0cf340e7c55e438c75dcf91c4e37b15cd5a
Durval Carvalho <dudurval2@gmail.com>
2021-08-01 16:27:06 -0300

    Add function to create boca description directory

--- src/commands/pack4.cpp
+++ src/commands/pack4.cpp
@@ -13,6 +13,7 @@
 #include "sh.h"
 #include "task.h"
 #include "util.h"
+#include "commands/genpdf.h"
 
 // Raw strings
 static const std::string help_message{
@@ -50,7 +51,7 @@ std::string help() { return usage() + help_message; }
  *             error code if a problem occurs.
  */
 int create_build_dirs(std::ostream &err) {
-    std::string boca_build_dir{CP_TOOLS_BUILD_DIR  + std::string("/boca/")};
+    std::string boca_build_dir{CP_TOOLS_BOCA_BUILD_DIR};
     auto res_create = fs::create_directory(boca_build_dir);
 
     if (not res_create.ok) {
@@ -66,6 +67,45 @@ int create_build_dirs(std::ostream &err) {
     return res_copy.rc;
 }
 
+int create_description_dir(std::ostream &out, std::ostream &err) {
+    std::string boca_desc_dir{CP_TOOLS_BOCA_BUILD_DIR  + std::string("description/")};
+
+    // Qual o nome do PDF deve ser dado? Nos exemplos do ribas é a letra da questão.
+    std::string pdf_name{"B.pdf"};
+    std::string opt_pdf_path{"--output=" + boca_desc_dir + pdf_name};
+
+    // Here I am using a CLI interface of the genpdf command to generate the PDF of the Boca package
+    char *argv[] = { (char*)"cp-tools",
+                     (char*)"genpdf",
+                     (char*) opt_pdf_path.c_str(),
+                     NULL
+                    };
+    auto res_pdf = genpdf::run(3, argv, out, err);
+
+    if(res_pdf != CP_TOOLS_OK) {
+        return res_pdf;
+    }
+
+    std::string problem_description{boca_desc_dir + std::string("desc.txt")};
+
+    // TODO: Como eu vou gerar um arquivo txt com os arquivos latex??
+
+    std::string problem_info{boca_desc_dir + std::string("problem.info")};
+
+    std::string problem_basename = "B";
+    std::string problem_fullname = "Beautiful Mountains";
+
+    std::string content{
+        std::string("basename=") + problem_basename + '\n' +
+        std::string("fullname=") + '\"' + problem_fullname + "\"\n" +
+        std::string("descfile=") + pdf_name + '\n'
+    };
+
+    fs::overwrite_file(problem_info, content);
+
+    return CP_TOOLS_OK;
+}
+
 int pack4_boca(std::ostream &out, std::ostream &err) {
 
     // Retornar int ou cptools::fs::Result?
@@ -73,6 +113,11 @@ int pack4_boca(std::ostream &out, std::ostream &err) {
     // Aqui não estou respeitando o padrão de sempre passar o out e o err
     // Na função não uso o out, e isso gera um -Werror=unused-parameter no build
     if(create_build_dirs(err) != CP_TOOLS_OK) {
+        // Qual erro deveria ser retornado aqui?
+        return -1;
+    }
+
+    if(create_description_dir(out, err) != CP_TOOLS_OK) {
         // Qual erro deveria ser retornado aqui?
         return -1;
     }
