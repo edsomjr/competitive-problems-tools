3ef98f00cf9168657eb3b0c0fce436e6d0b035f9
Durval Carvalho <dudurval2@gmail.com>
2021-08-01 16:26:45 -0300

    Add macro to boca build directory

--- include/dirs.h
+++ include/dirs.h
@@ -9,6 +9,7 @@
 
 #define CP_TOOLS_CLASSES_DIR   "/usr/local/lib/" NAME "/classes/"
 #define CP_TOOLS_BUILD_DIR     ".cp-build"
+#define CP_TOOLS_BOCA_BUILD_DIR  CP_TOOLS_BUILD_DIR "/boca/"
 #define CP_TOOLS_TEMP_DIR      ".cp-tmp"
 
 #define CP_TOOLS_CONFIG_FILE "." NAME "-config.json"
