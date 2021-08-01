94ba2d8a1e29672a6184ad493cfb4c1bccdcdd0d
Durval Carvalho <dudurval2@gmail.com>
2021-08-01 14:41:46 -0300

    Move the problem template to a specific directory

--- include/dirs.h
+++ include/dirs.h
@@ -4,6 +4,9 @@
 #include "defs.h"
 
 #define CP_TOOLS_TEMPLATES_DIR "/usr/local/lib/" NAME "/templates/"
+#define CP_TOOLS_PROBLEM_TEMPLATE_DIR CP_TOOLS_TEMPLATES_DIR "problem-template/"
+#define CP_TOOLS_BOCA_TEMPLATES_DIR CP_TOOLS_TEMPLATES_DIR "boca-package-structure/"
+
 #define CP_TOOLS_CLASSES_DIR   "/usr/local/lib/" NAME "/classes/"
 #define CP_TOOLS_BUILD_DIR     ".cp-build"
 #define CP_TOOLS_TEMP_DIR      ".cp-tmp"
