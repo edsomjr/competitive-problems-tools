a3d50cdba77cde256f7d1c02bd02b30b3d638dde
Vitor Falcão <vitorfhc@protonmail.com>
2021-08-14 17:49:38 +0000

    Add .old extension instead of replacing

--- src/conflicts.cpp
+++ src/conflicts.cpp
@@ -37,7 +37,7 @@ std::string solve_files(std::string local_file_name, std::string remote_file_nam
         fs::remove(local_file_path);
     } else if (different_hashes and equal_paths) {
         std::filesystem::path target(local_file_path);
-        target.replace_extension(".old");
+        target += ".old";
         cli::write(cli::message_type::warning, "The file '" + local_file_path.string() +
                                                    "' will be moved to '" + target.string() + "'");
         fs::copy(local_file_path, target);
